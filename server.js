620aaadf989056d8ff9fac7f30f87cae34c5ec76
code-thomasl <thomas.lefebvre01@etu.umontpellier.fr>
2020-01-29 13:21:18 +0100

    changes in server.js

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json()); 
 const port = process.env.PORT || 5000;
-const REDIS_PORT = process.env.PORT || 6379;
-let datastream;
-
 
 app.use(cors());
 
@@ -38,21 +35,6 @@ app.use((req, res, next) => {
 const corsOptions = {
   origin: 'http://localhost:3000'
 }
-
-/*
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-app.configure(function() {
-  app.use(allowCrossDomain);
-  //some other code
-}); 
-*/   
 
 const storeData = (data, path) => {
   try {
@@ -97,34 +79,10 @@ const randomWord = (length, file) => {
   let words = loadData()
 }
 
-// Cache middleware
-/*
-function cache(req, res, next) {
-  const { word } = req.params;
-
-  client.get(word, (err, data) => {
-    if(err) throw err;
-
-    if(data !== null) {
-      res.send(setResponse(word, data));
-    } else {
-      next();
-    }
-  })
-}
-*/
-
 // Set response
 function setResponse(word, wordData) {
   return wordData;
 }
-
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({
-//  extended: true
-//}));
-//app.use(express.json());
-//app.use(bodyParser.json());
 
 app.use(express.urlencoded())
 app.use(bodyParser.text());
@@ -151,15 +109,6 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-//Building routes
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
-
-
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
-
-
 app.get('/randomize', (req, res) => {
   let word = loadData('words-list.json');
 
@@ -167,17 +116,7 @@ app.get('/randomize', (req, res) => {
   console.log(word);
   //res.send(word[word.length-1]);
   res.send(word[getRandomInt(0, word.length-1)]);
-/*
-  if(fs.existsSync(`/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`)) {
-    console.log('File already exists!');
-    res.send(loadData(`/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`));
 
-  } else {
-    fetchWordFromDist(word);
-    //res.send(word);
-    res.send(loadData(`/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`));
-  }
-*/
   res.end()
 });
 
@@ -193,9 +132,6 @@ app.post('/sendform', async (req, res) => {
   if(fs.existsSync(`words_files/${word}.json`)) {
     console.log('File already exists!');
 
-    //Set data to Redis
-    //client.setx(word, 3600, parseFileSendProper(`/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`))
-
     res.send(parseFileSendProper(`words_files/${word}.json`));
 
   } else {
@@ -206,12 +142,6 @@ app.post('/sendform', async (req, res) => {
     await allInFetch(word, res);
     console.log('REPONSE FIN');
 
-    //res.send('Please click the Submit button to display result for : '+ word);
-    
-    //res.send('Please click SUBMIT to display');
-
-    //res.send(word);
-    //res.send(loadData(`/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`));
   }
 
 });
@@ -219,34 +149,6 @@ app.post('/sendform', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
 });
-/*
-app.post('/sendform', (req, res) => {
-  const word = req.body.name;
-  console.log(req.body);
-  console.log(`OK SOMETHING HAPPENING HERE`);
-  console.log(`Response from the form : ${word}`);
-  console.log(word);
-  //...
-  res.end()
-})
-*/
-
-//working function to fetch DATA from a word
-/*
-fetch(url)
-  .then(res => res.arrayBuffer())
-  .then(arrayBuffer => iconv.decode(new Buffer(arrayBuffer), 'iso-8859-1').toString())
-  .then(converted => {
-    const $ = cheerio.load(converted, { decodeEntities: false });
-    storeData($("CODE").text(), `/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`);
-
-    //console.log(converted)
-  }).catch(error => {
-    console.log(error);
-  });
-*/
-
-
 
 function parseWordFromDist(word) {
     const url = `http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=${word}`
@@ -256,8 +158,6 @@ function parseWordFromDist(word) {
     .then(arrayBuffer => iconv.decode(new Buffer(arrayBuffer), 'iso-8859-1').toString())
     .then(converted => {
       const $ = cheerio.load(converted, { decodeEntities: false });
-      //storeData($("CODE").html(), `/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`);
-      //storeData($("CODE").text(), `/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`);
       let def = /<def>(.|\n)*?<\/def>/gm;
       let rele = /(\d)/gm;
       let rels = /(\d)/gm;
@@ -281,7 +181,7 @@ const allInFetch = (word, res) => {
   .then(arrayBuffer => iconv.decode(new Buffer(arrayBuffer), 'iso-8859-1').toString())
   .then(converted => {
     const $ = cheerio.load(converted, { decodeEntities: false });
-    storeData($("CODE").html(), `/words_files/${word}.json`);
+    storeData($("CODE").html(), `words_files/${word}.json`);
     //storeData($("ClODE").text(), `/Users/thomaslefebvre/git/projet-jeudemot/backend/words_files/${word}.json`);
 
     return `words_files/${word}.json`
